817fbd58f68d82a0b7a8ad35b2618e21a3e9d7f5
aerithmoon <projectmoonalpha01@gmail.com>
2026-02-24 21:01:44 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 /* CONSOLIDATED RPG SCRIPT SYSTEM - REVISED */
 
 const CONFIG = {
-    csvUrl: 'csvUrl: 'csvUrl: 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTIz5r4BkhocZryD6Ju2UGYVWyEKXAvhhqS94Hm6-yMyegyjQM-MV6j0-mDnujDN72oLjPNCyfBlUsZ/pub?gid=0&single=true&output=csv',
+    csvUrl:'https://docs.google.com/spreadsheets/d/e/2PACX-1vTIz5r4BkhocZryD6Ju2UGYVWyEKXAvhhqS94Hm6-yMyegyjQM-MV6j0-mDnujDN72oLjPNCyfBlUsZ/pub?gid=0&single=true&output=csv',
     categories: ['Character', 'Monster', 'Pet', 'Item', 'Magic', 'Area']
 };
 
