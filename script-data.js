af03217aaba07c1cad4eb81dcdbbe59685beb292
aerithmoon <projectmoonalpha01@gmail.com>
2026-02-24 12:56:40 +0700

    Update script-data.js

--- script-data.js
+++ script-data.js
@@ -2,7 +2,7 @@
 
 const CONFIG = {
     // Replace with your CSV URL
-    csvUrl: 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTfX6o_W1y8q6v_r1R_S1p_S1p_S1p_S1p_S1p_S1p_S1p_S1p_S1p_S1p_S1p/pub?output=csv',
+    csvUrl: 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTIz5r4BkhocZryD6Ju2UGYVWyEKXAvhhqS94Hm6-yMyegyjQM-MV6j0-mDnujDN72oLjPNCyfBlUsZ/pub?output=csv',
     categories: ['Character', 'Monster', 'Pet', 'Item', 'Magic']
 };
 
